cd917da2f550af33d8046a485c6b87f327193efb
Speike-SHL <shao-haoluo@foxmail.com>
2022-08-02 15:43:27 +0800

    配置VSCODE

--- src/helloworld/scripts/helloworld_p.py
+++ src/helloworld/scripts/helloworld_p.py
@@ -1,7 +1,15 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   helloworld_p.py"
+@Time    :   2022/08/02 15:21:20"
+@Author  :   Speike 
+@Contact :   shao-haoluo@foxmail.com
+@Desc    :   None
+'''
 
 import rospy
 
 if __name__ == "__main__":
-    rospy.init_node("hellopy")
+    rospy.init_node("hello")
     rospy.loginfo("Hello world,I am python")
--- src/helloworld/src/helloworld_c.cpp
+++ src/helloworld/src/helloworld_c.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, char *argv[])
 {
+	//解决终端输出ROS_INFO出现乱码
+	setlocale(LC_ALL, "");
 	ros::init(argc, argv, "hello_node");
 	ROS_INFO("hello world,I am c++");
 	return 0;
